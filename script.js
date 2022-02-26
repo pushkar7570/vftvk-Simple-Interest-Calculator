efa48cfe21ef0d37303ac8dee79fa390079fd118
Pushkar <singh.pushkar7570@gmail.com>
2022-02-26 12:31:13 +0000

    simple interest calculator made from virtual thiea lab

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+//function to compute and output the interest line
+function compute() {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
+        "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
+        "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+        "in the year <mark>" + year + "</mark>";
 }
-        
+
+// function to update the interest slider dnamically
+function updateRate() {
+    document.getElementById("rate_val").innerHTML = document.getElementById("rate").value;
+}
+// function to validate the principal amount
+function validateAmount() {
+    var principal = document.getElementById("principal").value;
+    var biggerThanZero = parseInt(principal) > 0;
+    if (!biggerThanZero) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+}
